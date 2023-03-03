7b0d3c517ea56aa3153458070ea23dc160f0d8a6
Michel Mendoza <contacto@arycer.me>
2023-03-03 01:10:35 +0100

    Removed Ancient Debris from Nether terrain generation

--- src/main/java/com/arycer/permadeathfabric/difficultyChanges/day1/WorldGenerationRules.java
+++ src/main/java/com/arycer/permadeathfabric/difficultyChanges/day1/WorldGenerationRules.java
@@ -12,16 +12,21 @@ import net.minecraft.world.gen.feature.PlacedFeature;
 
 public class WorldGenerationRules {
     public void removeAncientDebris() {
+        // Define the keys for the ancient debris features
         RegistryKey<PlacedFeature> largeDebrisKey = RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier("minecraft", "ore_ancient_debris_large"));
         RegistryKey<PlacedFeature> smallDebrisKey = RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier("minecraft", "ore_debris_small"));
 
+        // Define the biome modification context
         BiomeModification biomeModification = BiomeModifications.create(new Identifier("permadeathfabric", "remove_ancient_debris"));
 
+        // Remove the ancient debris features from the nether
         biomeModification.add(ModificationPhase.REMOVALS, BiomeSelectors.foundInTheNether(), context -> {
-            context.getGenerationSettings().removeFeature(GenerationStep.Feature.UNDERGROUND_ORES, largeDebrisKey);
-            context.getGenerationSettings().removeFeature(GenerationStep.Feature.UNDERGROUND_ORES, smallDebrisKey);
+            // For some reason the ore features are also registered as underground decoration features so we need to remove them from both
             context.getGenerationSettings().removeFeature(GenerationStep.Feature.UNDERGROUND_DECORATION, largeDebrisKey);
             context.getGenerationSettings().removeFeature(GenerationStep.Feature.UNDERGROUND_DECORATION, smallDebrisKey);
+            // Remove the ore features from the ore generation step
+            context.getGenerationSettings().removeFeature(GenerationStep.Feature.UNDERGROUND_ORES, largeDebrisKey);
+            context.getGenerationSettings().removeFeature(GenerationStep.Feature.UNDERGROUND_ORES, smallDebrisKey);
         });
     }
 }
